85a3e550ccbaef96bd1419df1edf70b4f3f83b5c
Gillich Norbert <gillich.norbert@icloud.com>
2024-04-20 01:39:52 +0200

    Javítás

--- public/itemadd.js
+++ public/itemadd.js
@@ -3,44 +3,30 @@ const amountInput= document.querySelector("#itemAmount");
 const priceInput= document.querySelector("#itemPrice");
 const categoryInput= document.querySelector("#itemCategory");
 
-document.addEventListener('DOMContentLoaded', function () {
-    // A submit esemény figyelése a form elemen
-    const form = document.getElementById('addItemForm');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Az alapértelmezett működés megakadályozása
-
-    
-
-        // Az űrlap adatokának gyűjtése
-        const formData = new FormData(form);
-        const newItem = {
-            name: nameInput.value, // Módosítás: 'itemName' helyett 'name'
-            amount: amountInput.value, // Módosítás: 'itemAmount' helyett 'amount'
-            price: priceInput.value, // Módosítás: 'itemPrice' helyett 'price'
-            category: categoryInput.value // Módosítás: 'itemCategory' helyett 'category'
-        };
-
-        // POST kérés küldése a szervernek
-        fetch('/items', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newItem)
+function addEmployee() {
+    const url = MongoUri + "/" + dbname.collectionname;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: nameInput.value,
+            amount: amountInput.value,
+            price: priceInput.value,
+            category: categoryInput.value
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to add item');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Item added:', data);
-            // Esetlegesen frissítheted a felhasználói felületet az új elem hozzáadása után
-            // pl. újra lekérdezheted az összes elemet és megjelenítheted őket a felületen
-        })
-        .catch(error => console.error('Error adding item:', error));
-    });
-
-    // Egyéb kód a megjelenítéshez vagy egyéb funkciókhoz
-});
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to add item');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Employee added:', data);
+        // Esetlegesen frissítheted a felhasználói felületet az új elem hozzáadása után
+        // pl. újra lekérdezheted az összes elemet és megjelenítheted őket a felületen
+    })
+    .catch(error => console.error('Error adding employee:', error));
+}

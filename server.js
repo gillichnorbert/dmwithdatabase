a8bf1aac0cf0c6089369c8b1ea022dc1850d5482
Gillich Norbert <gillich.norbert@icloud.com>
2024-04-20 14:11:55 +0200

    Javítás

--- server.js
+++ server.js
@@ -92,3 +92,8 @@ app.put('/items/:id', async (req, res) => {
         console.log("MongoDB connection closed.");
     }
 });
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
